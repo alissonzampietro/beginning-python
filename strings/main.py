a1e6a7fa4bb953ada597ef208dd37455a88e19fc
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-04-16 00:38:19 +0100

    feat:A add features
    feat: add features

--- strings/main.py
+++ strings/main.py
@@ -1,7 +1,9 @@
 ## Strings
 name = "Marcosa"
 print('Show declared string: '+name)
-print('Define a range of the string to be showed: '+name[1:5])
+print('Range 1:5 showing string: '+name[1:5])
+print('Range :3 showing string: '+name[:3])
+print('Range 3: showing string: '+name[3:])
 
 # conta a quantidade de caracteres que uma string possui
 print('Show the size if the string using a method __len__():  '+str(name.__len__()))
@@ -9,3 +11,9 @@ print('Show the size if the string using a method len():  '+str(len(name)))
 
 #escaping
 print(r'C:\nome\nada')
+
+## Values assignement
+idade = 24
+tempo_moraria = 6
+nome = 'Alisson'
+print('%s tem %d anos e mora em portugal a %d meses' % (nome, idade, tempo_moraria))
