d30b1f4ef6aaeb583cc96149846b1b0422231793
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-04-22 02:39:01 +0100

    feat: add exception

--- input_test.py
+++ input_test.py
@@ -4,12 +4,12 @@ n = datetime.now()
 t = n.timetuple()
 y,m,d,h,m,s,wd,yd, isdst = t
 
-print(y)
+try:
 
-idade = int(input('Digite a sua idade: '))
-nome = input('Digite o seu nome: ')
+    idade = int(input('Digite a sua idade: '))
+    nome = input('Digite o seu nome: ')
+    year_born = str(y - idade)
+    print('Sr(a) '+nome+' o ano de seu nascimento é: '+year_born)
 
-year_born = str(y - idade)
-
-
-print('Sr(a) '+nome+' o ano de seu nascimento é: '+year_born)
+except:
+    print('Input inválidox')
