8070f5e65d7a19839c144717925fdd0fa555a1e8
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-04-22 01:06:09 +0100

    feat: creating first input interation

---
+++ input_test.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+n = datetime.now()
+t = n.timetuple()
+y,m,d,h,m,s,wd,yd, isdst = t
+
+print(y)
+
+idade = int(input('Digite a sua idade: '))
+nome = input('Digite o seu nome: ')
+
+year_born = str(y - idade)
+
+
+print('Sr(a) '+nome+' o ano de seu nascimento é: '+year_born)
