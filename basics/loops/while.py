dbed689766a30756699fe51359eec7c9f02d5ec3
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-04-27 21:08:03 +0100

    feat: while

---
+++ basics/loops/while.py
@@ -0,0 +1,14 @@
+import random
+
+top = 200
+
+number = random.randint(1,top)
+trials = 0
+
+while number < top:
+    # Can pass second argument to define how shown structure will behave
+    print(number, end=' ')
+    number += 1
+    trials += 1
+
+print('Was lefting just '+str(trials)+' to '+str(top))
