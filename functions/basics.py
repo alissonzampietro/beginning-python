62fcd4c8fab634f5f1936e76f08a590588d1be1a
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-05-12 23:48:42 +0100

    feat: add more methods

--- functions/basics.py
+++ functions/basics.py
@@ -1,24 +1,41 @@
 # Functions
 
 try:
+    onlyOneParameter = {'mod'}
     ## my fist function, it was really easy
     def dispatcher(functionName):
         allFunctions = {
             'sum':sum,
-            'multiply': multiply
+            'multiply': multiply,
+            'divide': divide,
+            'difference': difference,
+            'mod': mod,
         }
         return allFunctions[functionName]
 
     def sum(a,b):
         return a + b
+    
+    def difference(a,b):
+        return a - b
 
     def multiply(a,b):
         return a * b
+    
+    def divide(a,b):
+        return a / b
+    
+    def mod(a, b):
+        return a % 2
 
     def begin():
-        func = dispatcher(input('type the operation name: '))
+        funcName = input('type the operation name: ')
+        func = dispatcher(funcName)
         a = input('first param: ')
-        b = input('second param: ')
+        if(funcName not in onlyOneParameter):
+            b = input('second param: ')
+        else:
+            b = 0
         print(func(int(a),int(b)))
         begin()
 
