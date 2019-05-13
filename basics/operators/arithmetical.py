4c5add72a698c999854f4e3a059881c60fe078c4
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-05-14 00:50:55 +0100

    feat: creating operators

---
+++ basics/operators/arithmetical.py
@@ -0,0 +1,11 @@
+a = 6
+print('Multiply: '+str(4*4))
+a*=3
+print('Multiply and assign: '+str(a))
+print('Sum: '+str(4+4))
+print('Divide: '+str(4/3))
+print('Floor Divide: '+str(4//3))
+print('Mod: '+str(4%3))
+print('Exponential: '+str(4**4))
+a**=5
+print('Exponential and assign: '+str(a))
---
+++ basics/operators/membership.py
@@ -0,0 +1,4 @@
+
+pets = ['dog', 'cow', 'cat', 'bird']
+print('In: '+str('bir' in pets))
+print('Not in: '+str('bir' not in pets))
