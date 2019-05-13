f2de20eb250bbaecfeb4beda3efa3d807c884a96
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-05-13 01:21:00 +0100

    feat: recursive function

--- functions/recursive.py
+++ functions/recursive.py
@@ -1,3 +1,6 @@
+import datetime
+now = datetime.datetime.now()
+
 # Recursive function
 def factorial(number):
     if number == 0 or number == 1:
@@ -5,4 +8,12 @@ def factorial(number):
     
     return number * factorial(number-1)
 
+def calcAge(age=0):
+    return now.year - age
+
+# You can pass function as parameter
+def buildProfile(name, age, calcAge=calcAge):
+    print('Hello '+name+', i calculated that you waws born in '+str(calcAge(age)))
+
 print(factorial(5))
+print(buildProfile('Alisson', 24))
