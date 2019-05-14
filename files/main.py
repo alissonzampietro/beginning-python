6c90950c1e42613c154e522c8d252f76ef79cddf
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-05-14 23:40:51 +0100

    feat: add big texts

--- files/main.py
+++ files/main.py
@@ -1,3 +1,20 @@
-filee = open('names.txt', 'a')
+## if you use "w" if gonna overwrite everything that you have in the file
+# f = open('names.txt', 'w')
 
-print(filee)
+text = '''
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
+took a galley of type and scrambled it to make a type specimen book. 
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
+unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+including versions of Lorem Ipsum.
+'''
+
+## if you use "a" if gonna append to everything that you have in the file
+f = open('names.txt', 'a')
+f.write('machine learning')
+f.write('\n')
+f.write('learning with the best')
+f.write(text)
+f.close()
