a26ab0dd6df384b16c53f750c4874accd046c78e
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-05-27 23:42:42 +0100

    feat: add limit to the matrix

--- robot_game/Robot.py
+++ robot_game/Robot.py
@@ -5,16 +5,28 @@ class Robot:
         print(x,y)
 
     def moveUp(self):
-        self.y += 1
+        if(self.y < 10):
+            self.y += 1
+        else:
+            print('Não podes mover mais para cima')
 
     def moveDown(self):
-        self.y -= 1
+        if(self.y > 0):
+            self.y -= 1
+        else:
+            print('Não podes mover mais para baixo')
     
     def moveLeft(self):
-        self.x -= 1
+        if(self.x > 0):
+            self.x -= 1
+        else:
+            print('Não podes mover mais para esquerda')
 
     def moveRight(self):
-        self.x += 1
+        if(self.x < 10):
+            self.x += 1
+        else:
+            print('Não podes mover mais para direita')
 
     def showPosition(self):
         print(self.x,self.y)
