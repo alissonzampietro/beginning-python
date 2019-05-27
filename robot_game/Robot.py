79af3f437ab5ecc0bb731a85584d2199b70d6ded
Alisson Zampietro <alisson.zampietro@gmail.com>
2019-05-27 23:34:08 +0100

    feat: add robots movements

--- robot_game/Robot.py
+++ robot_game/Robot.py
@@ -1,3 +1,18 @@
 class Robot:
-    def __init__(self):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def moveUp(self, value):
+        self.y += value
+
+    def moveDown(self, value):
+        self.y -= value
+    
+    def moveLeft(self, value):
+        self.x -= value
+
+    def moveRight(self, value):
+        self.x += value    
+
         
