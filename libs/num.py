ed2a1df8c0dc09dd0148db82c46df819ae47ba36
Alisson Zampietro <AlissonZampietro.DECSKILL@edp.pt>
2019-07-23 23:28:37 +0100

    feat: add new itens

--- libs/num.py
+++ libs/num.py
@@ -9,3 +9,19 @@ print(type(a))
 ## The bth arrays should have the same amount of values
 twoMatrix = numpy.array([[2,4,6,8,10,12],[1,3,5,7,9,11]])
 print(twoMatrix)
+
+## accessing values
+print(twoMatrix[1][0])
+
+## [ 2  4  6  8 10 12]
+print(twoMatrix[0,:])
+
+## [ 1  3  5  7  9 11]
+print(twoMatrix[1,:])
+
+
+## get size on total (12)
+print('total of itens: '+str(numpy.size(twoMatrix)))
+
+## [4 3]
+print(twoMatrix[:,1])
