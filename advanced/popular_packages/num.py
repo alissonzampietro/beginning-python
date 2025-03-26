5211331ca44cf6ec7ce8aa958231e5c147e2afc8
Alisson Zampietro <alisson.zampietro@gmail.com>
2025-03-26 17:39:09 +0000

    feat: reshaping numbers

--- advanced/popular_packages/num.py
+++ advanced/popular_packages/num.py
@@ -11,3 +11,8 @@ print(np.random.choice([1,2,3,4,5]))
 
 # picking 3 random values of the list
 print(np.random.choice(["i","love","python", "and", "computers", "datasets", "machine learning"], 3))
+
+# reshaping numbers
+arranging_list = np.array([1,2,3,4,5,6,7,8,9,10])
+print(arranging_list.reshape(2,5))
+
--- advanced/popular_packages/pandas_example.py
+++ advanced/popular_packages/pandas_example.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 list = pd.Series([1,2,3,4,5])
 print(list)
@@ -8,4 +9,9 @@ dataframe = pd.DataFrame({
     'column2': [6,7,8,9,10]
 })
 print(dataframe)
+
+
+
+
+df = pd.DataFrame(np.random.randint(0,100, (10,10)))
 
